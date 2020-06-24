1d48cdfb009b9f5861755870b9fa490860d2de53
anne constance pund <ac.pund@gmail.com>
2020-06-24 11:53:12 +0200

    edit path correction show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -12,6 +12,18 @@ class Booking < ApplicationRecord
     end 
   end
 
+  def depose
+    if self.floor_type == 'carrelage'
+      return 50*self.surface_area
+    elsif self.product_type == 'parquet massif'
+      return 40*self.surface_area
+    elsif self.product_type == 'parquet stratifier'
+      return 30*self.surface_area 
+    else
+      return 0*self.surface_area
+    end
+  end 
+
   def total
     return self.labor + self.product.price_per_square_meter * self.surface_area + 50
   end 
